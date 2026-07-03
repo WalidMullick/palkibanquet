705dee09443dfb2a93accab8f50ee69e3bee4f96
Walid Mullick <walidmullick@github.com>
2026-07-03 14:44:00 +0530

    Deploy: clean build output at root

---
+++ deploy.bat
@@ -0,0 +1,12 @@
+@echo off
+cd /d d:\palki-deploy
+git rm -rf .
+xcopy /E /I /Y "D:\Palki Website\dist\assets" "assets\"
+copy /Y "D:\Palki Website\dist\index.html" index.html
+copy /Y "D:\Palki Website\dist\config.json" config.json
+copy /Y "D:\Palki Website\dist\favicon.svg" favicon.svg
+copy /Y "D:\Palki Website\dist\icons.svg" icons.svg
+copy /Y "D:\Palki Website\dist\.nojekyll" .nojekyll
+git add .
+git commit -m "Deploy: clean build output at root"
+git push origin gh-pages
